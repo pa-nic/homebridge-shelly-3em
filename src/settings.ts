34ec5623373133526af34d2985e0c5a38ee3c5a7
Patrick <52470102+pa-nic@users.noreply.github.com>
2026-02-02 18:56:10 +0100

    package renaming

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'Shelly3EM-HomebridgePlugin';
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-shelly-3em-plugin';
+export const PLUGIN_NAME = 'homebridge-shelly-3em';
